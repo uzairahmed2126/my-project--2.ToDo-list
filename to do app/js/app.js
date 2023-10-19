3a6f20a5efc7ac798dcaa324984ab7f9670af7fd
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-10-19 18:11:09 +0530

    completed

--- to do app/js/app.js
+++ to do app/js/app.js
@@ -1,62 +1,123 @@
-const todoInput = document.querySelector('.todo-input')
-const todoButton = document.querySelector('.todo-button')
-const todoList = document.querySelector('.todo-list')
-const h1El = document.querySelector('h1')
-todoList.addEventListener('click', deleteCheck)
-todoButton.addEventListener('click', addBtn)
-const filterOption = document.querySelector('.filter-todo')
-filterOption.addEventListener('click',filterOpt)
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
+// events
+document.addEventListener("DOMContentLoaded", getTodos);
+todoList.addEventListener("click", deleteCheck);
+todoButton.addEventListener("click", addBtn);
+filterOption.addEventListener("click", filterTodo);
 
 function addBtn(event) {
-    event.preventDefault()
-    // create div
-    const todoDiv = document.createElement('div')
-    todoDiv.classList.add('todo')
-    // create li
-    const newTodo = document.createElement('li')
-    newTodo.innerHTML = todoInput.value
-    newTodo.classList.add('todo-item')
-    todoDiv.appendChild(newTodo)
-    const completeButton = document.createElement('button')
-    completeButton.innerHTML = `<i class="fas fa-check"></i>`
-    completeButton.classList.add('complete-btn')
-    todoDiv.appendChild(completeButton)
-    const trashButton = document.createElement('button')
-    trashButton.innerHTML = `<i class="fas fa-trash"></i>`
-    trashButton.classList.add('trash-btn')
-    todoDiv.appendChild(trashButton)
-    todoList.appendChild(todoDiv)
-    if (todoInput.value == '') {
-        h1El.innerHTML = 'Enter the value'
-        h1El.classList.add('active')
-    }
-    if (addBtn) {
-        // const trashBtn = document.querySelector('.fa-trash')
-        // trashBtn.addEventListener('click', trashList)
-        todoInput.value = ''
-        h1El.style.color = ''
-        h1El.classList.remove('active')
-        h1El.innerHTML = `Uzair's ToDo List`
-    }
+  event.preventDefault();
+  // create div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  // create li
+  const newTodo = document.createElement("li");
+  newTodo.innerHTML = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  saveLocalTodos(todoInput.value);
+  const completeButton = document.createElement("button");
+  completeButton.innerHTML = `<i class="fas fa-check"></i>`;
+  completeButton.classList.add("complete-btn");
+  todoDiv.appendChild(completeButton);
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  todoList.appendChild(todoDiv);
+  todoInput.value = "";
 }
-
 
 function deleteCheck(e) {
-    const item = e.target
-    if (item.classList[0] === 'trash-btn') {
-        const todo = item.parentElement;
-        todo.classList.add('fall')
-        todo.addEventListener('transitionend', function () {
-            todo.remove()
-        })
-    }
-    // check mark
-    if (item.classList[0] === 'complete-btn') {
-        const todo = item.parentElement
-        todo.classList.toggle('completed')
-    }
+  const item = e.target;
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    removeLocalTodos(todo);
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+  // check mark
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
 
-function filterOpt(e){
-console.log(e.target)
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach((todo) => {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "none";
+        } else {
+          todo.style.display = "flex";
+        }
+    }
+  });
+}
+
+function saveLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getTodos() {
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach((todo) => {
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    // create li
+    const newTodo = document.createElement("li");
+    newTodo.innerHTML = todo;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    const completeButton = document.createElement("button");
+    completeButton.innerHTML = `<i class="fas fa-check"></i>`;
+    completeButton.classList.add("complete-btn");
+    todoDiv.appendChild(completeButton);
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    todoList.appendChild(todoDiv);
+  });
+}
+
+function removeLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex = todo.children[0].innerHTML;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
